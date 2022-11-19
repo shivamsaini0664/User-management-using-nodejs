bd16d7ddca6a9b573dcf18a5ce05f0c9f2a1da72
shivamsaini0664 <shivam0664.be20@chitkara.edu.in>
2022-11-19 15:11:44 +0530

    Added user.json
    setup the package and installed nodemon and express

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const application = express();
 
 
 application.use(express.json());
-application.use(express.urlencoded({ extended: true}));
+application.use(express.urlencoded({ extended: true }));
 
-application.get('/users', (req,res) => {
-    filesystem.readFile("./user.json","utf-8",(error,data)=> {
-        if(error){
+application.get('/users', (req, res) => {
+    filesystem.readFile("./user.json", "utf-8", (error, data) => {
+        if (error) {
             throw error;
         }
         console.log(JSON.parse(data));
@@ -17,47 +17,50 @@ application.get('/users', (req,res) => {
 });
 
 
-application.get('/users/:id', (req,res) => {
-    filesystem.readFile("./user.json","utf-8",(error,data)=> {
-        if(error){
+application.get('/user/:id', (req, res) => {
+    filesystem.readFile("./user.json", "utf-8", (error, data) => {
+        if (error) {
             throw error;
         }
-        const require_id=req.params.id;
-        let specific_user_data= [];
+        // console.log("data = ", data);
+        const require_id = req.params.id;
+        // console.log("require user data = ",require_id);
+        let specific_user_data = [];
         let parse_data = JSON.parse(data);
-        for( let user  of parse_data){
-            if(user.id == require_id) {
+        for (let user of parse_data) {
+            if (user.id == require_id) {
                 specific_user_data.push(user);
-                
+
             }
         }
-        console.log(JSON.parse(specific_user_data));
-        res.send(JSON.parse(specific_user_data));
+        console.log("specific user data = ",specific_user_data);
+        // console.log("type of specific user data", typeof(specific_user_data));
+        res.send(specific_user_data);
     });
 });
 
-application.delete('/users/:id', (req,res) => {
-    filesystem.readFile("./user.json","utf-8",(error,data)=> {
-        if(error){
+application.delete('/user/:id', (req, res) => {
+    filesystem.readFile("./user.json", "utf-8", (error, data) => {
+        if (error) {
             throw error;
         }
         const require_id = req.params["id"];
         let deleted_data = [];
         let parse_data = JSON.parse(data);
-        for(let user of parse_data){
-            if(user.id == require_id){
-                
+        for (let user of parse_data) {
+            if (user.id == require_id) {
+
             }
-            else{
+            else {
                 deleted_data.push(user);
             }
         }
         let stringifydata = JSON.stringify(deleted_data);
-        filesystem.writeFile("./user.json",stringifydata,"utf-8",(error)=> {
-            if(error){
+        filesystem.writeFile("./user.json", stringifydata, "utf-8", (error) => {
+            if (error) {
                 throw error;
             }
-            else{
+            else {
                 console.log(JSON.parse(stringifydata));
                 res.send(JSON.parse(stringifydata));
             }
@@ -65,30 +68,30 @@ application.delete('/users/:id', (req,res) => {
     });
 });
 
-application.put('/users/:id', (req,res) => {
-    filesystem.readFile("./user.json","utf-8",(error,data)=> {
-        if(error){
+application.put('/user/:id', (req, res) => {
+    filesystem.readFile("./user.json", "utf-8", (error, data) => {
+        if (error) {
             throw error;
         }
-        const require_id = req.params["id"];
-        
+        const require_id = req.params.id;
+
         let parse_data = JSON.parse(data);
-        for(let user of parse_data){
-            if(user.id == require_id){
-                user.name=req.body.name;
-                user.address=req.body.address;
-                user.pincode=req.body.pincode;
-                user.state=req.body.state;
-                user.hobby=req.body.hobby;
+        for (let user of parse_data) {
+            if (user.id == require_id) {
+                user.name = req.body.name;
+                user.address = req.body.address;
+                user.pincode = req.body.pincode;
+                user.state = req.body.state;
+                user.hobby = req.body.hobby;
             }
         }
 
         let stringifydata = JSON.stringify(parse_data);
-        filesystem.writeFile("./user.json",stringifydata,"utf-8",(error)=> {
-            if(error){
+        filesystem.writeFile("./user.json", stringifydata, "utf-8", (error) => {
+            if (error) {
                 throw error;
             }
-            else{
+            else {
                 console.log(JSON.parse(stringifydata));
                 res.send(JSON.parse(stringifydata));
             }
@@ -96,27 +99,27 @@ application.put('/users/:id', (req,res) => {
     });
 });
 
-application.post('/user', (req,res) => {
-filesystem.readFile("./user.json","utf-8",(error,data)=> {
-    if(error){
-        throw error;
-    }
-    let parse_data=JSON.parse(data);
-    let new_user_request=req.body;
-    let random_id = Date.now().toString();
-    new_user_request.id=random_id;
-    parse_data=[new_user_request,...parse_data]
-    let stringifydata = JSON.stringify(parse_data);
-    filesystem.writeFile("./user.json",stringifydata,"utf-8",(error)=> {
-        if(error){
+application.post('/user', (req, res) => {
+    filesystem.readFile("./user.json", "utf-8", (error, data) => {
+        if (error) {
             throw error;
         }
-        else{
-            console.log(JSON.parse(stringifydata));
-            res.send(JSON.parse(stringifydata));
-        }
+        let parse_data = JSON.parse(data);
+        let new_user_request = req.body;
+        let random_id = Date.now().toString();
+        new_user_request.id = random_id;
+        parse_data = [new_user_request, ...parse_data]
+        let stringifydata = JSON.stringify(parse_data);
+        filesystem.writeFile("./user.json", stringifydata, "utf-8", (error) => {
+            if (error) {
+                throw error;
+            }
+            else {
+                console.log(JSON.parse(stringifydata));
+                res.send(JSON.parse(stringifydata));
+            }
+        });
     });
-});
 });
 
 application.listen(3096, () => console.log(`streaming on PORT ${3096}!`));
